9f175e35c076e79924e5cbe4eb154ffa4351961e
Fatih Kucuk <1fatihkucuk@gmail.com>
2020-04-11 23:36:48 +0300

    Add axios and handle image upload

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [file, setFile] = useState(null);
+
   const fileChangeHandler = (e) => {
     console.log(e.target.files);
+    setFile(e.target.files[0]);
+  };
+
+  const uploadHandler = () => {
+    const formData = new FormData();
+    formData.append('image', file, file.name);
+    axios
+      .post('http://localhost:3000/api/v1/upload', formData)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
 
   return (
     <div className="App">
       <input type="file" onChange={fileChangeHandler}></input>
+      {file && <button onClick={uploadHandler}>Upload</button>}
     </div>
   );
 }
