8c62e0820f92e711c67e177e2a470abcc9226c3a
Fatih Kucuk <1fatihkucuk@gmail.com>
2020-04-12 20:34:34 +0300

    Add material UI library

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import { API_URL } from './constants';
+import { DropzoneArea } from 'material-ui-dropzone';
+import { Button } from '@material-ui/core';
 
 function App() {
   const [images, setImages] = useState([]);
-  const [file, setFile] = useState(null);
-  const [imagePath, setImagePath] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   useEffect(() => {
     getImages();
@@ -20,18 +21,18 @@ function App() {
       })
       .catch((err) => alert(err));
   };
-  const fileChangeHandler = (e) => {
-    setFile(e.target.files[0]);
+  const fileChangeHandler = (files) => {
+    setSelectedFile(files[0]);
   };
 
   const uploadHandler = () => {
     const formData = new FormData();
-    formData.append('image', file, file.name);
+    formData.append('image', selectedFile, selectedFile.name);
     axios
       .post(`${API_URL}/upload`, formData)
       .then((res) => {
-        setImagePath(`${res.data.path}`);
         setImages([...images, res.data]);
+        setSelectedFile(null);
       })
       .catch((err) => console.log(err));
   };
@@ -50,8 +51,35 @@ function App() {
 
   return (
     <div className="App">
-      <input type="file" onChange={fileChangeHandler}></input>
-      {file && <button onClick={uploadHandler}>Upload</button>}
+      <div style={{ display: 'inline-block', width: 500, margin: 20 }}>
+        <DropzoneArea
+          showAlerts={false}
+          filesLimit={1}
+          onChange={fileChangeHandler}
+        />
+      </div>
+      {/* <div>
+        {file && (
+          <button
+            onClick={uploadHandler}
+            style={{
+              display: 'inline-block',
+              width: 500,
+              padding: 10,
+              fontSize: 16,
+              cursor: 'pointer',
+            }}>
+            Upload
+          </button>
+        )}
+      </div> */}
+      <div>
+        {selectedFile && (
+          <Button variant="contained" onClick={uploadHandler}>
+            Upload
+          </Button>
+        )}
+      </div>
       <div>{imageList}</div>
     </div>
   );
