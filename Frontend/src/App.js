a43e9aa7d5e72b4988c941199c8c081530021dd0
Fatih Kucuk <1fatihkucuk@gmail.com>
2020-04-12 13:44:04 +0300

    Add get images method on page load

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
+  const [images, setImages] = useState([]);
   const [file, setFile] = useState(null);
   const [imagePath, setImagePath] = useState(null);
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  const getImages = () => {
+    axios
+      .get('http://localhost:3000/api/v1/upload')
+      .then((res) => {
+        setImages(res.data);
+      })
+      .catch((err) => alert(err));
+  };
 
   const fileChangeHandler = (e) => {
     setFile(e.target.files[0]);
@@ -17,21 +31,25 @@ function App() {
       .post('http://localhost:3000/api/v1/upload', formData)
       .then((res) => {
         setImagePath(`http://localhost:3000/api/v1/${res.data.path}`);
+        setImages([...images, res.data]);
       })
       .catch((err) => console.log(err));
   };
+
+  const imageList = images.map((image) => {
+    return (
+      <img
+        key={image._id}
+        src={image.path}
+        style={{ width: 200, height: 200, margin: 20 }}></img>
+    );
+  });
 
   return (
     <div className="App">
       <input type="file" onChange={fileChangeHandler}></input>
       {file && <button onClick={uploadHandler}>Upload</button>}
-      {imagePath && (
-        <div>
-          <img
-            src={imagePath}
-            style={{ width: 200, height: 200, marginTop: 20 }}></img>
-        </div>
-      )}
+      {<div>{imageList}</div>}
     </div>
   );
 }
